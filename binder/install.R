98b00f8e0e88d6d54602365340af125ff4396c27
Frank M Gonzales <31703957+fmgonzales@users.noreply.github.com>
2019-04-19 20:29:55 -0700

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -4,6 +4,63 @@ install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
+install.packages("caret") 
+install.packages("dplyr") 
+install.packages("dbplyr") 
+install.packages("lubridate") 
+install.packages("randomforest") 
+install.packages("tidyverse") 
+
+install.packages("forecast")
+install.packages("prophet")
+install.packages("tidyverse")
+
+install.packages("data.table")
+install.packages("dygraphs")
+install.packages("xts")
+
+install.packages("highcharter")
+install.packages("zoo")
+
+install.packages("tidyquant")
+install.packages("timeDate")
+
+install.packages("plotly")
+install.packages("ggfortify")
+install.packages("tseries")
+install.packages("gridExtra")
+install.packages("docstring")
+install.packages("here")
+
+install.packages("rgdal")
+install.packages("sp")
+install.packages("leaflet")
+install.packages("maps")
+install.packages("Hmisc")
+install.packages("owmr")
+
+install.packages("htmltools")
+install.packages("TimeProjection")
+
+install.packages("readr")
+install.packages("ggridges")
+install.packages("corrplot")
+install.packages("survival")
+install.packages("OIsurv")
+install.packages("ROCR")
+
+install.packages("quantregForest")
+install.packages("e1071")
+install.packages("tree")
+install.packages("MASS")
+install.packages("mgcv")
+
+install.packages("pastecs")
+install.packages("lattice")
+
+
+
+
 ### install bioconductor packages
 # install.packages("BiocManager")
 # BiocManager::install("package")
